919011f28349987a89f48112f950976a8a8bceb8
ethanchen <ethanyc216@gmail.com>
2019-05-16 13:36:59 +0800

    add

---
+++ include/caffe/layers/fsp_layer.hpp
@@ -0,0 +1,44 @@
+#ifndef CAFFE_FSP_LAYER_HPP_
+#define CAFFE_FSP_LAYER_HPP_
+
+#include <vector>
+
+#include "caffe/blob.hpp"
+#include "caffe/layer.hpp"
+#include "caffe/proto/caffe.pb.h"
+
+namespace caffe {
+
+/**
+ * @brief add by zhanggang03@baidu.com
+ * Implement the paper A Gift from Knowledge Distillation:
+ * Fast Optimization, Network Minimization and Transfer Learning
+ */
+template <typename Dtype>
+class FSPLayer : public Layer<Dtype> {
+ public:
+  explicit FSPLayer(const LayerParameter& param)
+      : Layer<Dtype>(param) {}
+  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+
+  virtual inline const char* type() const { return "FSP"; }
+  virtual inline int MinBottomBlobs() const { return 2; }
+  virtual inline int ExactNumTopBlobs() const { return 1; }
+
+ protected:
+  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+};
+
+}  // namespace caffe
+
+#endif  // CAFFE_FSP_LAYER_HPP_
